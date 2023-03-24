11e1103b97d739563801da04454330f9f2db058a
Thomas Alberto <tom.alberto@yahoo.co.uk>
2023-03-24 13:43:09 +0000

    Added docstring to one_rep_max function in ORMCalculator

--- StrengthStandards/ORMCalculator/views.py
+++ StrengthStandards/ORMCalculator/views.py
@@ -8,6 +8,8 @@ def OneRepMaxCalculator(request):
 
 
 def one_rep_max(request):
+    '''Function one_rep_max takes the post information in one_rep_max.html, and returns a render of the
+    one_rep_max_result.html page with the results of the calculate_one_rep_max function defined in calculator.py'''
     one_rep_max_value = None
     if request.method == 'POST':
         weight = float(request.POST.get('weight'))
